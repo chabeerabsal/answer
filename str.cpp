1d1b20c80c14a52c4e2b259fbd8dd0712e1817da
chabeerabsal <chabeerabsal@gmail.com>
2023-01-10 14:55:27 -0500

    add'

--- str.cpp
+++ str.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
-#include<string.h>
+#include<string>
+#include<vector>
 using namespace std;
+//vector<string>o;
+vector<string>pattern(string p,string s)
+{
+	if(s.empty())
+	{
+	vector<string>o;
+	 o.push_back(p);
+	return o;   
+	}
+	char c=s.at(0);
+     vector<string>s1=pattern(p+c,s.substr(1));
+    vector<string>s2=pattern(p,s.substr(1));
+s1.insert(s1.end(),s2.begin(),s2.end());
+return s1;   
+	
+}
 int main()
 {
-	string s="iam chabeer";
-	cout<<s.length();
-	return 0;
+	//vector<string>o;
+   vector<string>o=pattern(" ","abc");
+   for(int i=0;i<o.size();i++)
+   cout<<o[i];
 }
